eb282abdf7942f2f53d2a0cc3b6f6256555e2b21
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2024-04-08 19:10:25 +0200

    Modified build_docker_images.sh to find pmacct in root project folder, ran 200:00, 300:00, 400:00 (passed)

--- test-framework/tools/pmacct_build/build_docker_images.sh
+++ test-framework/tools/pmacct_build/build_docker_images.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-# Temporary solution until we integrate the framework in the pmacct repo
-# Rootless containers --> discuss with Paolo
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
 
-PMACCT_LOCATION="$SCRIPT_DIR/pmacct"
-if [ ! -d "$PMACCT_LOCATION" ]; then
-  echo "Error: $PMACCT_LOCATION does not exist."
-  echo
-  echo "     !!! Please clone the pmacct repo in $SCRIPT_DIR !!!"
-  echo
-  exit 1
-fi
+PMACCT_LOCATION="$SCRIPT_DIR/../../../"
 
 TAG='_build'
 
 echo "Building pmacct docker images"
-docker build -t pmacct-base:$TAG -f $SCRIPT_DIR/base/Dockerfile $SCRIPT_DIR/pmacct || exit $?
+docker build -t pmacct-base:$TAG -f $SCRIPT_DIR/base/Dockerfile $PMACCT_LOCATION || exit $?
 
-docker build -t nfacctd:$TAG -f $SCRIPT_DIR/nfacctd/Dockerfile_non_root $SCRIPT_DIR/pmacct || exit $?
-docker build -t pmbmpd:$TAG -f $SCRIPT_DIR/pmbmpd/Dockerfile_non_root $SCRIPT_DIR/pmacct || exit $?
-docker build -t pmbgpd:$TAG -f $SCRIPT_DIR/pmbgpd/Dockerfile_non_root $SCRIPT_DIR/pmacct || exit $?
+docker build -t nfacctd:$TAG -f $SCRIPT_DIR/nfacctd/Dockerfile_non_root $PMACCT_LOCATION || exit $?
+docker build -t pmbmpd:$TAG -f $SCRIPT_DIR/pmbmpd/Dockerfile_non_root $PMACCT_LOCATION || exit $?
+docker build -t pmbgpd:$TAG -f $SCRIPT_DIR/pmbgpd/Dockerfile_non_root $PMACCT_LOCATION || exit $?
+
