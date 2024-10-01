7c8c0ed26d3dbb8e59783d5d6ec11c28881bd4cd
Maxence Younsi <mx.yns@outlook.fr>
2024-10-01 11:22:22 +0200

    pmacct-gauze docker build

--- test-framework/tools/pmacct_build/build_docker_images.sh
+++ test-framework/tools/pmacct_build/build_docker_images.sh
@@ -5,8 +5,26 @@ PMACCT_ROOT_LOCATION="$SCRIPT_DIR/../../../"
 
 TAG='_build'
 
-echo "Building pmacct docker images"
-docker build --build-arg NUM_WORKERS=$(nproc) -t base:$TAG -f $PMACCT_ROOT_LOCATION/docker/base/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
+
+if "$PMACCT_ROOT_LOCATION/config.status" --config | grep -q "enable-pmacct-gauze"; then
+
+  echo "Building pmacct docker images with pmacct-gauze"
+  PMACCT_GAUZE_ROOT_LOCATION=$(realpath "$PMACCT_ROOT_LOCATION/docker")
+  if [ ! -d "$PMACCT_GAUZE_ROOT_LOCATION/pmacct-gauze" ]; then
+    echo "Error: $PMACCT_GAUZE_ROOT_LOCATION/pmacct-gauze does not exist."
+    echo
+    echo "     !!! Please clone the pmacct repo in $PMACCT_GAUZE_ROOT_LOCATION !!!"
+    echo
+    exit 1
+  fi
+
+  docker build --build-arg NUM_WORKERS=$(nproc) -t base:$TAG -f $PMACCT_ROOT_LOCATION/docker/base/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
+else
+
+  echo "Building pmacct docker images"
+  docker build --build-arg NUM_WORKERS=$(nproc) -t base:$TAG -f $PMACCT_ROOT_LOCATION/docker/pmacct-gauze-base/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
+fi
+
 docker build --build-arg NUM_WORKERS=$(nproc) -t nfacctd:$TAG -f $PMACCT_ROOT_LOCATION/docker/nfacctd/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
 docker build --build-arg NUM_WORKERS=$(nproc) -t pmacctd:$TAG -f $PMACCT_ROOT_LOCATION/docker/pmacctd/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
 docker build --build-arg NUM_WORKERS=$(nproc) -t pmbgpd:$TAG -f $PMACCT_ROOT_LOCATION/docker/pmbgpd/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
